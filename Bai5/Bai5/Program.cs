544acb7629f30b3e58198de8f4c5613f42813acc
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2026-02-12 15:30:18 +0700

    add bai 5

---
+++ Bai5/Bai5/Program.cs
@@ -0,0 +1,41 @@
+﻿int a, b;
+char kytu;
+do
+{
+    Console.Write("Nhap so thu nhat: ");
+    a = int.Parse(Console.ReadLine());
+    Console.Write("Nhap phep toan + - * /: ");
+    kytu = char.Parse(Console.ReadLine());
+    Console.Write("Nhap so thu hai: ");
+    b = int.Parse(Console.ReadLine());
+
+    if (kytu == '+')
+    {
+        Console.WriteLine($"Ket qua la: {a + b}");
+        break;
+    }
+    else if (kytu == '-')
+    {
+        Console.WriteLine($"Ket qua la: {a - b}");
+        break;
+    }
+    else if (kytu == '*')
+    {
+        Console.WriteLine($"Ket qua la: {a * b}");
+        break;
+    }
+    else if (kytu == '/')
+    {
+        Console.WriteLine($"Ket qua la: {(float)a / b}");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Vui long chi nhap + - * /");
+    }
+} while (kytu != '+' || kytu != '-' || kytu != '*' || kytu != '/');
+
+
+
+
+
