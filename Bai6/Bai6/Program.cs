f90c59051c90d4cea1939a15a38020420465bae0
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2026-02-14 21:44:44 +0700

    add bai 6 va bai 7

---
+++ Bai6/Bai6/Program.cs
@@ -0,0 +1,8 @@
+﻿
+//bai6
+double pi = 3.14, r;
+Console.Write("Nhap ban kinh: ");
+r = double.Parse(Console.ReadLine());
+Console.WriteLine($"Chu vi hinh tron la {2 * pi * r} ");
+Console.WriteLine($"Dien tich hinh tron la {r*r*pi}");
+
---
+++ Bai7/Bai7/Program.cs
@@ -0,0 +1,7 @@
+﻿//bai7
+int x, y;
+for (y = -5; y <= 5; y++)
+{
+    x = y * y - 2 * y + 1;
+    Console.WriteLine("y = {0}; x = {0} * {0} - 2 * {0} + 1 = {1}", y, x);
+}
