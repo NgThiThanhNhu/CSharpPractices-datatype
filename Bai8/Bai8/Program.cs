1ce44be905a5d802d40136a289ac5fc527ef96da
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2026-02-14 22:43:37 +0700

    add bai 8

---
+++ Bai8/Bai8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Nhap quang duong: ");
+float s = float.Parse(Console.ReadLine());
+Console.Write("Nhap thoi gian: ");
+float t = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Van toc la {(s / t):F3} m/s");
+
+Console.WriteLine($"Van toc la {((s / t) * 3.6):F3} km/h");
+Console.WriteLine($"Van toc la {((s / t) * 2.237):F3} miles/h");
