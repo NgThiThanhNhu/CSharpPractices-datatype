547612101b49ec12b66afa46f0e18c2344ab2536
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2026-02-14 23:19:37 +0700

    add bai 9

---
+++ Bai9/Bai9/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Nhap ban kinh R: ");
+double R = double.Parse(Console.ReadLine());
+double Pi = 3.14;
+Console.WriteLine($"Dien tich hinh cau: {(4 * Pi * (R * R)):F3}");
+Console.WriteLine($"The tich hinh cau: {(4 / 3 * Pi * (R * R * R)):F3}");
