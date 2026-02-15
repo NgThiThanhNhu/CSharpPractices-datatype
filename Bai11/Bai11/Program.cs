a9838016c2beb713a4e72b5b663d0af59a7b4c28
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2026-02-15 11:40:11 +0700

    add bai 11

---
+++ Bai11/Bai11/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Nhap mot ky tu bat ki: ");
+char kt = char.Parse(Console.ReadLine());
+
+if (kt == 'a' || kt == 'o' || kt == 'e' || kt == 'u' || kt == 'i')
+{
+    Console.WriteLine("ky tu vua nhap la nguyen am");
+}else if (kt >= '0' && kt <= '9')
+{
+    Console.WriteLine("ky tu vua nhap la chu so");
+}
+else
+{
+    Console.WriteLine("Ky tu vua nhap khong phai la nguyen am cung khong phai chu so");
+}
