63096053cf7348dbf6d60cb9b1e87121fd4200a3
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2026-02-15 11:22:42 +0700

    add bai 10

---
+++ Bai10/Bai10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Nhap so a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Nhap so b: ");
+int b = int.Parse(Console.ReadLine());
+
+bool ketqua = ((a % 2 == 0)&&(b % 2 == 0)) ? true : false;
+Console.WriteLine((ketqua) ? "Ca hai deu la so chan" : "Hai so khong deu la so le");
